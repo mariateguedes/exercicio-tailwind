a0fd4a3c9115752746ad6e2782aa12851e979110
mariateguedes <mariateguedes@gmail.com>
2022-04-29 10:08:27 -0300

    iniciando o projeto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className=" flex bg-back w-screen h-screen">
+    <div className="container mx-auto bg-surface shadow border p-8">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'surface': '#121212',
+        'base': '#ffffff',
+        'surface-low': '#888888',
+        'back': '#ececec',
+        'surface-high': '#d0d0d0',
+        'neutral': '#f5f6f8',
+        'primary-neutral-low': '#7b7c7d',
+        'primary-neutral-high': '#323947',
+        'primary-neutral-lowest': '#b1b2b4',
+        'primary': '#1a1b1f',
+        'primary-base-low': '#5f6063',
+        'primary-base-lowest': '#b8b8b8'
+      },
+    },
+  },
+  plugins: [],
+}
